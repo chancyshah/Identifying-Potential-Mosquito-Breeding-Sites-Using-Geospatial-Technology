ea1c0ec78fafdda25be24933a1f3cc7384640df6
Chancy Shah <72352809+chancyshah@users.noreply.github.com>
2022-01-18 16:11:15 +0530

    Update Surat.py

--- Surat.py
+++ Surat.py
@@ -9,11 +9,57 @@ import streamlit as st
 import geemap
 import ee
 
+ee.Initialize()
+
+#Drainage = ee.Image.loadGeoTIFF('https://github.com/chancyshah/Identifying-Potential-Mosquito-Breeding-Sites-Using-Geospatial-Technology/blob/7be95bb2b7a2aa0afebafca1a8e8f7cdcc764cc2/Buffer_Drainage.tif')
+
+Month = ['January','February', 'March', 'April' ,'May' ,'June','July' ,'August' ,'September' ,'October','November' ,'December' ]
+
+st.title('Identifying Potential Mosquito Breeding Sites Using Geospatial Technology')
+
+add_selectbox1 = st.sidebar.selectbox( "Select City",("Surat"," "))
+
+add_selectbox2 = st.sidebar.selectbox( "Select Year",("2020", " "))
+year = int(add_selectbox2)
+add_selectbox3 = st.sidebar.select_slider(
+     'Select Month', options= Month)
+index = Month.index(add_selectbox3)
+index = index + 1
+
+if st.button("Dispaly Potential Mosquito Breeding Sites"):
+    Mosquito(index,year,'Map')
+else:
+    Map = geemap.Map(center=[21.1702,72.8311], zoom=11)
+    #Map.addLayer(CityFile, {}, 'Cities')
+    Map.to_streamlit(width=600, height=600, responsive=True, scrolling=False)
+    
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: rgb(	255,235,205);
+}</style>""", unsafe_allow_html=True)
+
+
+with st.expander("About Web App"):
+     st.write("""In recent years, vector-borne diseases (VBD) have emerged as a serious public health problem in most of the countries including India. India aims to develop 100 smart cities, and therefore planning the health infrastructure while limiting human-to-human interactions, preserving vulnerable populations, and managing infrastructure while honing capacity to leverage smart technology for healing masses, is the cornerstone of a smart city.
+              The disordered urban growth that may favor the emergence of the mosquito in cities is a problem of increasing magnitude. 
+              Vector borne diseases constitute a major public health problem in the list of communicable diseases. The most important are malaria, dengue fever, chikungunya fever. Geospatail Technology is a powerful tool to analyse the distribution of mosquitoes and their relationship to different environmental factors, and can substantially improve our ability to quantify the impacts of demographic, climatic and ecological changes in vector distribution.""")
+              
+with st.expander("Web App Developer"):
+     st.write("***Pranav Pandya***")
+     st.write("M.Tech (Geoinformatics) B.Tech (Civil)")
+     st.write("Email: pranav.pandya@flame.edu.in")
+     st.write("[Linkedin] (https://www.linkedin.com/in/pranavspandya/)")
+     st.write("***Chancy Shah***")
+     st.write("M.Sc (Geoinformatics) B.Tech (Civil)")
+     st.write("Email: shahchancy28@gmail.com")
+     st.write("[Website] (https://sites.google.com/view/chancyshah/home) , [Linkedin](https://www.linkedin.com/in/chancy-shah-671787119/)")  
+      
 def Mosquito(month,year,describe):
   ###Data Collection
   year=2020
   
-  CityFile = geemap.geojson_to_ee("C:/Users/India/Surat_Ward.geojson" , geodesic=False, encoding='utf-8')
+  CityFile = geemap.geojson_to_ee("https://github.com/chancyshah/Identifying-Potential-Mosquito-Breeding-Sites-Using-Geospatial-Technology/blob/7be95bb2b7a2aa0afebafca1a8e8f7cdcc764cc2/Surat%20Ward.geojson" , geodesic=False, encoding='utf-8')
   
   dataset = ee.ImageCollection("LANDSAT/LE07/C01/T1_32DAY_NDVI").filterBounds(CityFile).filter(ee.Filter.calendarRange (year,year, 'year')).filter(ee.Filter.calendarRange (month,month+1, 'month'));
   NDVI = dataset.mosaic().clip(CityFile);
@@ -92,51 +138,4 @@ def Mosquito(month,year,describe):
   Map.addLayer(y,{'palette': ['red']},'Potential Site')
   Map.addLayer(CityFile, {'color': 'black', 'fillColor': '00000000'}, 'Ward Boundary', opacity = 0.4)
   Map.to_streamlit(width=600, height=600, responsive=True, scrolling=False)
- 
-
-ee.Initialize()
-
-#Drainage = ee.Image.loadGeoTIFF('C:/Users/India/Buffer_Drainage.tif')
-
-Month = ['January','February', 'March', 'April' ,'May' ,'June','July' ,'August' ,'September' ,'October','November' ,'December' ]
-
-st.title('Identifying Potential Mosquito Breeding Sites Using Geospatial Technology')
-
-add_selectbox1 = st.sidebar.selectbox( "Select City",("Surat"," "))
-
-add_selectbox2 = st.sidebar.selectbox( "Select Year",("2020", " "))
-year = int(add_selectbox2)
-add_selectbox3 = st.sidebar.select_slider(
-     'Select Month', options= Month)
-index = Month.index(add_selectbox3)
-index = index + 1
-
-if st.button("Dispaly Potential Mosquito Breeding Sites"):
-    Mosquito(index,year,'Map')
-else:
-    Map = geemap.Map(center=[21.1702,72.8311], zoom=11)
-    #Map.addLayer(CityFile, {}, 'Cities')
-    Map.to_streamlit(width=600, height=600, responsive=True, scrolling=False)
-    
-m = st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: rgb(	255,235,205);
-}</style>""", unsafe_allow_html=True)
-
-
-with st.expander("About Web App"):
-     st.write("""In recent years, vector-borne diseases (VBD) have emerged as a serious public health problem in most of the countries including India. India aims to develop 100 smart cities, and therefore planning the health infrastructure while limiting human-to-human interactions, preserving vulnerable populations, and managing infrastructure while honing capacity to leverage smart technology for healing masses, is the cornerstone of a smart city.
-              The disordered urban growth that may favor the emergence of the mosquito in cities is a problem of increasing magnitude. 
-              Vector borne diseases constitute a major public health problem in the list of communicable diseases. The most important are malaria, dengue fever, chikungunya fever. Geospatail Technology is a powerful tool to analyse the distribution of mosquitoes and their relationship to different environmental factors, and can substantially improve our ability to quantify the impacts of demographic, climatic and ecological changes in vector distribution.""")
-              
-with st.expander("Web App Developer"):
-     st.write("***Pranav Pandya***")
-     st.write("M.Tech (Geoinformatics) B.Tech (Civil)")
-     st.write("Email: pranav.pandya@flame.edu.in")
-     st.write("[Linkedin] (https://www.linkedin.com/in/pranavspandya/)")
-     st.write("***Chancy Shah***")
-     st.write("M.Sc (Geoinformatics) B.Tech (Civil)")
-     st.write("Email: shahchancy28@gmail.com")
-     st.write("[Website] (https://sites.google.com/view/chancyshah/home) , [Linkedin](https://www.linkedin.com/in/chancy-shah-671787119/)")  
-                
+       
